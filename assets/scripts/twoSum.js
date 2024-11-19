df8e32363fac389717e8d893005d6f01aef7ecb2
rafidrahman1 <rafid0001@gmail.com>
2024-11-19 17:11:32 +0600

    s

---
+++ assets/scripts/twoSum.js
@@ -0,0 +1,19 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+function twoSum(nums, target) {
+  const arr = [];
+  for (let n = 0; n < nums.length; n++) {
+    for (let i = n + 1; i < nums.length; i++) {
+      if (nums[n] + nums[i] === target) {
+        arr.push(n, i);
+        return arr;
+      }
+    }
+  }
+  return arr;
+}
+
+console.log(twoSum([1, 2, 3, 4, 5], 9));
